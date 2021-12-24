31dd378cadc35ea25f4342b7e93ed42e7a07decc
Emmanuel rapoo <aniemmanuelrapo@gmail.com>
2021-12-24 03:57:12 +0100

    dashboard, sidebar and clients component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import './App.css';
 import Appnavbar from './components/layout/AppNavbar';
+import { BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
+import Dashboard from './components/layout/Dashboard';
 
 function App() {
   return (
-    <div className="App">
-      <Appnavbar />
-      <div className="container">
-         <h1>Hello</h1>
+    <Router>
+      <div className="App">
+        <Appnavbar />
+        <div className="container">
+        <Switch>
+            <Route exact path="/"><Dashboard /></Route>
+            {/* <Route exact path="/about"><About /></Route>
+            <Route exact path="/test"><Test /></Route>
+            <Route><NotFound /></Route> */}
+          </Switch>
+        </div>
       </div>
-     
-    </div>
+    </Router>
   );
 }
 
