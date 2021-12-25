48cfd43ebef850fe884e0d515e21355545fc8b5d
Emmanuel rapoo <aniemmanuelrapo@gmail.com>
2021-12-25 03:56:38 +0100

    edited app.js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,23 @@
 import './App.css';
 import Appnavbar from './components/layout/AppNavbar';
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { Provider } from 'react-redux'
-import store from './store';
+import { BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
 import Dashboard from './components/layout/Dashboard';
 
 function App() {
   return (
-    <Provider store={store}>
-      <Router>
-        <div className="App">
-          <Appnavbar />
-          <div className="container">
-          <Switch>
-              <Route exact path="/"><Dashboard /></Route>
-              {/* <Route exact path="/about"><About /></Route>
-              <Route exact path="/test"><Test /></Route>
-              <Route><NotFound /></Route> */}
-            </Switch>
-          </div>
+    <Router>
+      <div className="App">
+        <Appnavbar />
+        <div className="container">
+        <Switch>
+            <Route exact path="/"><Dashboard /></Route>
+            {/* <Route exact path="/about"><About /></Route>
+            <Route exact path="/test"><Test /></Route>
+            <Route><NotFound /></Route> */}
+          </Switch>
         </div>
-      </Router>
-    </Provider>
+      </div>
+    </Router>
   );
 }
 
